87f791755e3412c1e651c132e604eea40ce7d43c
Misael Ntirushwa <misaelntirushwa@gmail.com>
2021-07-09 11:54:35 -0400

    added a shared library

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  projects: ['<rootDir>/apps/staff-directory'],
+  projects: ['<rootDir>/apps/staff-directory', '<rootDir>/libs/shared'],
 };
---
+++ libs/shared/src/lib/shared.ts
@@ -0,0 +1,10 @@
+export interface EmployeeSummary {
+    avatar: string;
+    firstName: string; 
+    lastName: string;
+    title: string;
+    location: string;
+    phone: string;
+    cellPhone: string;
+    email: string;
+}
